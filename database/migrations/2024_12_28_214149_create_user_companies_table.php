742aec8b1484b4932d54eeafe7e3f63a6b214c7d
RozKoy <okkoy.1401@gmail.com>
2024-12-29 21:21:55 +0700

    feat(migration n model): user company

---
+++ database/migrations/2024_12_28_214149_create_user_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_companies', function (Blueprint $table): void {
+            $table->uuid('id')->primary();
+
+            // REQUIRED
+            $table->string('role', 100);
+
+            // FOREIGN KEY
+            $table->foreignUuid('company_id')->constrained('companies');
+            $table->foreignUuid('user_id')->constrained('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_companies');
+    }
+};
