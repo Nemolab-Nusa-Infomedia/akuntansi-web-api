5653d85f0b19e60b740ea696c86d02b9e5e7fda3
RozKoy <okkoy.1401@gmail.com>
2024-12-29 21:25:10 +0700

    feat(migration n model): otp

---
+++ database/migrations/2024_12_29_212235_create_otps_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('otps', function (Blueprint $table): void {
+            $table->uuid('id')->primary();
+
+            // REQUIRED
+            $table->char('code', 6);
+
+            // FOREIGN KEY
+            $table->foreignUuid('user_id')->constrained('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('otps');
+    }
+};
