dd23741e2a0b526680c12513a88811207a54bda5
RozKoy <okkoy.1401@gmail.com>
2024-12-30 09:44:58 +0700

    feat(endpoint): login

---
+++ resources/views/mail/otp.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>Gunakan kode OTP untuk melanjutkan proses anda</h1>
+
+    <p>Kode : <b>{{ $otp }}</b></p>
+</div>
--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,15 @@
 <?php
+
+use App\Http\Controllers\Authentication\LoginController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('auth')->group(function (): void {
+	Route::post('login', [LoginController::class, 'action']);
+});
